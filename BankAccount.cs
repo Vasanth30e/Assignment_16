8791e0eb00f887ccdc55a2bfc06ab5362fe3071f
unknown <vasanthgamer2000@gmail.com>
2023-07-26 15:28:21 +0530

    Bank Account with Properties

---
+++ BankAccount.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_16
+{
+    public class BankAccount
+    {
+        private int accountNumber = 1;
+ 
+        public int AccountNumber { get; }
+        
+        private string AccountHolderName { get; }
+
+        private double Balance { get; set; }
+
+        public BankAccount(string accountHolderName)
+        {
+            AccountNumber = accountNumber++;
+            AccountHolderName = accountHolderName;
+            Balance = 0;
+        }
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public bool Withdrawal(double amount)
+        {
+            if(Balance >= amount)
+            {
+                Balance -= amount;
+                return true;
+            }  
+            return false;
+            
+        }
+
+        public void PrintAccountDetails()
+        {
+            Console.WriteLine("Account Number: " + AccountNumber);
+            Console.WriteLine("Account Holder Name: " + AccountHolderName);
+            Console.WriteLine("Balance: " +  Balance);
+            Console.WriteLine("\n");
+        }
+    }
+
+    
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Account Holder Name");
+            string accountHolder = Console.ReadLine();
+            BankAccount account = new BankAccount(accountHolder);
+
+            while (true)
+            {
+                Console.WriteLine("Select the Process \n1. Deposit\n2. Withdrawal\n3. Exit\nEnter your Choice");
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the Depost Amount:");
+                        double depositAmount = double.Parse(Console.ReadLine());
+                        account.Deposit(depositAmount);
+                        account.PrintAccountDetails();
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter the Withdrawal Amount:");
+                        double withdrawalAmount = double.Parse(Console.ReadLine());
+                        if (account.Withdrawal(withdrawalAmount))
+                        {
+                            Console.WriteLine("Withdrawal Successfull");
+                            account.PrintAccountDetails();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Insufficient Balance");
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine("Exiting the Process");
+                        return;                        
+                    default:                        
+                        Console.WriteLine("\nWrong Choice\nPlease Enter the Right Choice\n");                       
+                        break;
+                }
+                
+            }
+            
+        }
+    }
+}
